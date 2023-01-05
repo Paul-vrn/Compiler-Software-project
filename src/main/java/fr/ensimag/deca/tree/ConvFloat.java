bdf59d93c4ff74eb35fb53858df989d4515e8580
Paul Vernin <paul.vernin@gmail.com>
2023-01-05 11:17:14 +0100

    fix(DecaParser) : IfThenElse recursive

--- src/main/java/fr/ensimag/deca/tree/ConvFloat.java
+++ src/main/java/fr/ensimag/deca/tree/ConvFloat.java
@@ -5,6 +5,7 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.EnvironmentExp;
 
+
 /**
  * Conversion of an int into a float. Used for implicit conversions.
  * 

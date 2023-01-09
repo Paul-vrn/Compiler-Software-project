a2797098e3237ba89af2eb80993e7768e3186f35
Paul Bonmariage <paul.bonmariage@gmail.com>
2023-01-09 16:09:10 +0100

    Add float operations and tests for arith and bool expressions

--- src/main/java/fr/ensimag/deca/tree/Divide.java
+++ src/main/java/fr/ensimag/deca/tree/Divide.java
@@ -27,14 +27,24 @@ public class Divide extends AbstractOpArith {
         getLeftOperand().codeGenExpr(compiler, n);
         if (n < Register.RMAX) {
             getRightOperand().codeGenExpr(compiler, n + 1);
-            compiler.addInstruction(new QUO(Register.getR(n+1), Register.getR(n)));
+            if(getLeftOperand().getType().isFloat() || getRightOperand().getType().isFloat()){
+                compiler.addInstruction(new DIV(Register.getR(n+1), Register.getR(n)));
+            }
+            else{
+                compiler.addInstruction(new QUO(Register.getR(n+1), Register.getR(n)));
+            }
 
         } else {
             compiler.addInstruction(new PUSH(Register.getR(n)));
             getRightOperand().codeGenExpr(compiler, n);
             compiler.addInstruction(new LOAD(Register.getR(n), Register.R0));
             compiler.addInstruction(new POP(Register.getR(n)));
-            compiler.addInstruction(new QUO(Register.R0, Register.getR(n)));
+            if(getLeftOperand().getType().isFloat() || getRightOperand().getType().isFloat()){
+                compiler.addInstruction(new DIV(Register.R0, Register.getR(n)));
+            }
+            else{
+                compiler.addInstruction(new QUO(Register.R0, Register.getR(n)));
+            }
         }
     }
 

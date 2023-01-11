6d10ac8e50b256069b3b4abe9269ba0a2fe86a78
Paul Vernin <paul.vernin@gmail.com>
2023-01-11 14:51:44 +0100

    feat(codegen) : TSTO in arith op

--- src/main/java/fr/ensimag/deca/tree/Multiply.java
+++ src/main/java/fr/ensimag/deca/tree/Multiply.java
@@ -25,9 +25,11 @@ public class Multiply extends AbstractOpArith {
 
         } else {
             compiler.addInstruction(new PUSH(Register.getR(n)));
+            compiler.getMemory().increaseTSTO();
             getRightOperand().codeGenExpr(compiler, n);
             compiler.addInstruction(new LOAD(Register.getR(n), Register.R0));
             compiler.addInstruction(new POP(Register.getR(n)));
+            compiler.getMemory().decreaseTSTO();
             compiler.addInstruction(new MUL(Register.R0, Register.getR(n)));
         }
         if (this.getType().isFloat())
